850d33f4290a5ff7ac1210b2d09f9b908654cdf2
Vien <levien26092k1@gmail.com>
2023-12-27 12:21:23 +0700

    ui login

--- next-14/src/app/login/page.tsx
+++ next-14/src/app/login/page.tsx
@@ -1,35 +1,72 @@
 "use client";
 import { authStore } from "@/stores/auth.store";
-
+import facebook from "../../../facebook.svg"
 const login = () => {
   return (
-    <div className="flex justify-center items-center bg-gradient-to-r from-orange-300 via-slate-300 to-orange-300 h-screen">
-      <div className="flex flex-col items-center w-full sm:max-w-sm md:max-w-[500px] xl:max-w-xl h-[50%] md:h-[70%] bg-white rounded-xl overflow-hidden p-2">
-        {/* logo */}
-        <h2>
-          <span className="text-center">
-            <strong>Logo</strong>
-          </span>
-        </h2>
-        <div className="flex flex-col justify-center items-center space-y-10 w-[80%] h-full pb-2">
-          <button
-            onClick={() => authStore.signIn()}
-            className="w-full h-[50px] bg-slate-200"
-          >
-            Sign In
-          </button>
-          <button
-            onClick={() => authStore.signUp()}
-            className="w-full h-[50px] bg-slate-200"
-          >
-            Sign Up
-          </button>
-          <button
-            onClick={() => authStore.signOut()}
-            className="w-full h-[50px] bg-slate-200"
-          >
-            Sign Out
-          </button>
+    <div className="flex justify-center items-center bg-gradient-to-r from-orange-300 via-slate-300 to-orange-300 h-screen p-2">
+      <div className="flex flex-col items-center justify-between w-full h-auto md:max-w-[500px] xl:max-w-xl bg-white rounded-xl overflow-hidden p-5">
+        <div className="flex w-full h-[50px] bg-slate-200 rounded-xl overflow-hidden p-1 space-x-2">
+          <div className="flex-1 h-full bg-white rounded-xl p-2 text-center">
+            <h2>Sign in</h2>
+          </div>
+          <div className="flex-1 h-full rounded-xl p-2 text-center text-[#787D86]">
+            <h2>Register</h2>
+          </div>
+        </div>
+        {/* form */}
+        <div className="flex-1 w-full space-y-5 my-10">
+          <div className="flex flex-col h-[100px]">
+            <label htmlFor="email" className="text-2xl my-2">
+              Email
+            </label>
+            <input
+              id="email"
+              className="h-[50px] outline-none border-[1px] rounded-xl border-[#787D86] border-solid p-2"
+            ></input>
+          </div>
+          <div className="flex flex-col h-[100px]">
+            <label htmlFor="password" className="text-2xl my-2">
+              Password
+            </label>
+            <input
+              id="password"
+              className="h-[50px] outline-none border-[1px] rounded-xl border-[#787D86] border-solid p-2"
+            ></input>
+          </div>
+        </div>
+        {/* actions */}
+        <div className=" flex flex-col w-full space-y-5 my-10 align-bottom">
+          {/* action button */}
+          <div className="flex justify-center items-center w-full h-[50px] bg-black rounded-lg text-white">
+            <p className="text-xl">Login</p>
+          </div>
+          <div className="flex justify-center items-center w-full h-[50px] bg-black rounded-lg text-white">
+            <p className="text-xl">Forgot password</p>
+          </div>
+          {/* social button*/}
+          <div className="flex justify-center items-center w-full h-[50px] space-x-5">
+            <div className="w-[50px] h-[50px]">
+              <img
+                src={`../../../github.svg`}
+                className="w-full h-full"
+                alt="github"
+              />
+            </div>
+            <div className="w-[50px] h-[50px]">
+              <img
+                src={`../../../facebook.svg`}
+                className="w-full h-full"
+                alt="facebook"
+              />
+            </div>
+            <div className="w-[50px] h-[50px]">
+              <img
+                src={`../../../google.svg`}
+                className="w-full h-full"
+                alt="google"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
