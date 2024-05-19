ed86d347f34a3c88d23dcf45ee3c15f4e69a0676
Vien <levien26092k1@gmail.com>
2024-05-20 01:49:48 +0700

    feature crud room

--- next-14/src/app/(admin)/dashboard/page.tsx
+++ next-14/src/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Button, Col, DatePicker, Divider, Drawer, Form, Input, Modal, Row, Select, Space, Table, Upload, UploadFile, GetProp, UploadProps } from "antd";
+import { Button, Col, DatePicker, Divider, Drawer, Form, Input, Modal, Row, Select, Space, Table, Upload, UploadFile, GetProp, UploadProps, notification, Spin } from "antd";
 import { SearchProps } from "antd/es/input";
 import { EditOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
-import { useState } from "react";
-import { Option } from "antd/es/mentions";
+import { useEffect, useState } from "react";
+import DrawerRoom from "./room/page";
+import { deleteRoom, readRoom } from "./room/actions";
 export interface IRoom {
 	key: string,
 	id: string,
@@ -26,197 +27,122 @@ const getBase64 = (file: FileType): Promise<string> =>
 const HomeDashboard = () => {
 	const [open, setOpen] = useState(false);
 	const [typeDrawer, setTypeDrawer] = useState<string>('');
-	const [previewOpen, setPreviewOpen] = useState(false);
-	const [previewImage, setPreviewImage] = useState('');
+	const [dataSource, setDataSource] = useState<any[]>([])
+	const [dataList, setDataList] = useState<any[]>([])
+	const [isLoading, setIsloading] = useState(false)
+	const [roomEdit, setRoomEdit] = useState()
+	const [stringSearch, setStringSearch] = useState('')
 	const { Search } = Input;
-	const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
-	const [fileList, setFileList] = useState<UploadFile[]>([
-	]);
+	const onSearch: SearchProps['onSearch'] = (value, _e, info) => setStringSearch(value.trim());
 
-	const handlePreview = async (file: UploadFile) => {
-		if (!file.url && !file.preview) {
-			file.preview = await getBase64(file.originFileObj as FileType);
-		}
+	const resultReadRoom = async () => {
+		setIsloading(true)
+		const { data, error } = await readRoom()
+		const cleanData = data?.map((room) => { return { ...room, key: room.id } })
+		cleanData && setDataSource(cleanData)
+		cleanData && setDataList(cleanData)
+		setIsloading(false)
+	}
 
-		setPreviewImage(file.url || (file.preview as string));
-		setPreviewOpen(true);
-	};
 
-	const handleChange: UploadProps['onChange'] = ({ fileList: newFileList }) =>
-		setFileList(newFileList);
+	useEffect(() => {
+		resultReadRoom()
+	}, [])
 
-	const uploadButton = (
-		<button style={{ border: 0, background: 'none' }} type="button">
-			<PlusOutlined />
-			<div style={{ marginTop: 8 }}>Upload</div>
-		</button>
-	);
-
-	const showDrawerCreate = () => {
+	const showDrawerCreate = async () => {
 		setTypeDrawer('Create')
 		setOpen(true);
 	};
-	const showDrawerEdit = () => {
+	const showDrawerEdit = async (room: any) => {
+		setRoomEdit(room)
 		setTypeDrawer('Edit')
 		setOpen(true);
 	};
-	const onClose = () => {
+	const onClose = async () => {
 		setOpen(false);
+		await resultReadRoom()
 	};
 
-	const dataSource: IRoom[] = [
-		{
-			key: '1',
-			id: 'room',
-			price: 32,
-			size: '10 m^2',
-			guest: 1,
-			image_url: '',
-		}, {
-			key: '2',
-			id: 'room_2',
-			price: 32,
-			size: '10 m^2',
-			guest: 1,
-			image_url: '',
-		},
-	];
+	const handleDeleteRoom = async (id: string) => {
+		setIsloading(true)
+		const resultDelete = await deleteRoom(id)
+		if (resultDelete.error?.message) {
+			notification.error({ message: resultDelete.error?.message })
+		}
+		else {
+			notification.success({ message: "Created succesfull!" })
+		}
+		await resultReadRoom()
+	}
 
 	const columns = [
-		{
-			title: 'image_url',
-			dataIndex: 'image_url',
-			key: 'image_url',
-			className: "w-[24%]"
-		},
 		{
 			title: 'Room No',
 			dataIndex: 'id',
 			key: 'id',
 		},
 		{
+			title: 'Image Link',
+			dataIndex: 'image_url',
+			key: 'image_url',
+			className: "max-w-[300px] truncate overflow-hidden"
+		},
+		{
+			title: 'Description',
+			dataIndex: 'description',
+			key: 'description',
+			className: "max-w-[300px] truncate overflow-hidden"
+		},
+		{
 			title: 'Price/Day ($)',
 			dataIndex: 'price',
 			key: 'price',
-			className: "w-[14%]"
 		},
 		{
 			title: 'size',
 			dataIndex: 'size',
 			key: 'size',
-			className: "w-[14%]"
 		},
 		{
 			title: 'guest',
 			dataIndex: 'guest',
 			key: 'guest',
-			className: "w-[14%]"
 		},
 		{
 			title: 'Action',
 			render: (_: any, record: any) => {
 				return <div className="flex gap-3 w-[40px]">
-					<Button onClick={showDrawerEdit}><EditOutlined /></Button>
-					<Button><DeleteOutlined /></Button>
+					<Button onClick={() => showDrawerEdit(record)}><EditOutlined /></Button>
+					<Button onClick={() => handleDeleteRoom(record.id)}><DeleteOutlined /></Button>
 				</div>
 			}
 		},
 	];
-	return (<section className="w-full h-full">
-		<div className="flex h-12 items-center px-2 justify-between">
-			<h3 className="text-xl font-bold">Room management</h3>
-			<Search placeholder="Search by room number" onSearch={onSearch} style={{ width: 250 }} />
-		</div>
-		<Divider></Divider>
-		<div className="flex text-md font-bold my-5">
-			<Button onClick={showDrawerCreate}>Add New Room</Button>
-		</div>
-		<Table dataSource={dataSource} columns={columns} />
-		<Drawer
-			title={typeDrawer === 'Create' ? "Create New Room" : "Edit Room"}
-			width={720}
-			onClose={onClose}
-			open={open}
-			styles={{
-				body: {
-					paddingBottom: 80,
-				},
-			}}
-			extra={
-				<Space>
-					<Button onClick={onClose}>Cancel</Button>
-					<Button onClick={onClose}>
-						Submit
-					</Button>
-				</Space>
-			}
-		>
-			<Form layout="vertical" >
-				<Row gutter={16}>
-					<Col span={24}>
-						<Form.Item
-							name="id"
-							label="Room No.: "
-							rules={[{ required: true, message: 'Please enter Room No.' }]}
-						>
-							<Input placeholder="Please enter Room No." />
-						</Form.Item>
-					</Col>
-				</Row>
-				<Row gutter={16}>
-					<Col span={24}>
-						<Form.Item
-							name="price"
-							label="Price/day ($): "
-							rules={[{ required: true, message: 'Please enter Price/day ($)' }]}
-						>
-							<Input type='number' placeholder="Please enter Price/day ($)" />
-						</Form.Item>
-					</Col>
-				</Row>
-				<Row gutter={16}>
-					<Col span={24}>
-						<Form.Item
-							name="guest"
-							label="Guest: "
-							rules={[{ required: true, message: 'Please enter number of Guest' }]}
-						>
-							<Input type='number' placeholder="Please enter number of Guest" />
-						</Form.Item>
-					</Col>
-				</Row>
-				<Row gutter={16}>
-					<Col span={24}>
-						<Form.Item
-							name="size"
-							label="Size: "
-							rules={[{ required: true, message: 'Please enter Size' }]}
-						>
-							<Input type='number' placeholder="Please enter Size" />
-						</Form.Item>
-					</Col>
-				</Row>
-				<Row>
-					<Col span={25}>
-						<Form.Item
-							name="img_url"
-							label="Image: "
-						>
-							<Upload
-								action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-								listType="picture-card"
-								fileList={fileList}
-							// onPreview={handlePreview}
-							// onChange={handleChange}
-							>
-								{fileList.length >= 8 ? null : uploadButton}
-							</Upload>
-						</Form.Item>
-					</Col>
-				</Row>
-			</Form>
-		</Drawer>
-	</section>
+
+	useEffect(() => {
+		if (stringSearch) {
+			let filterRoom = dataSource.filter((room) => String(room.id).includes(stringSearch))
+			setDataList(filterRoom)
+		}
+		else if (stringSearch === '' && dataSource.length !== 0) {
+			setDataList(dataSource)
+		}
+	}, [stringSearch])
+
+	return (<Spin spinning={isLoading}>
+		<section className="w-full h-full">
+			<div className="flex h-12 items-center px-2 justify-between">
+				<h3 className="text-xl font-bold">Room management</h3>
+				<Search placeholder="Search by room number" onSearch={onSearch} style={{ width: 250 }} allowClear />
+			</div>
+			<Divider></Divider>
+			<div className="flex text-md font-bold my-5">
+				<Button onClick={showDrawerCreate}>Add New Room</Button>
+			</div>
+			<Table dataSource={dataList} columns={columns} />
+			{open && <DrawerRoom title={typeDrawer === 'Create' ? "Create New Room" : "Edit Room"} dataEdit={roomEdit} onClose={onClose} open={open} />}
+		</section>
+	</Spin>
 	)
 }
 
---
+++ next-14/src/app/(admin)/dashboard/room/page.tsx
@@ -0,0 +1,193 @@
+import React from 'react'
+import { Button, Col, Drawer, Form, Input, Row, Space, UploadFile, GetProp, UploadProps, Spin, notification } from "antd";
+import { SearchProps } from "antd/es/input";
+import { EditOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import { useState } from "react";
+import { createRoom, updateRoom } from './actions';
+
+type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
+const getBase64 = (file: FileType): Promise<string> =>
+	new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => resolve(reader.result as string);
+		reader.onerror = (error) => reject(error);
+	});
+
+const DrawerRoom = (props: any) => {
+	const { title, onClose, open, dataEdit } = props
+	const [isLoading, setIsLoading] = useState(false)
+	const [fileList, setFileList] = useState<UploadFile[]>([]);
+	const { TextArea } = Input;
+	const [form] = Form.useForm()
+
+	const handleAddRoom = async (value: any) => {
+		setIsLoading(true)
+		const result = await createRoom(value)
+		if (result.error?.message) {
+			notification.error({ message: result.error?.message })
+		}
+		else {
+			notification.success({ message: "Created succesfull!" })
+		}
+		setIsLoading(false)
+		form.resetFields()
+		onClose()
+	}
+
+	const handleUpdateRoom = async (value: any) => {
+		setIsLoading(true)
+		const resultUpdateRoom = await updateRoom(value)
+		if (resultUpdateRoom?.error?.message) {
+			notification.error({ message: 'This Room Id is already exist!' })
+		}
+		else {
+			notification.success({ message: 'Update Successful!' })
+		}
+		setIsLoading(false)
+		form.resetFields()
+		onClose()
+	}
+
+	return (
+		<Drawer
+			title={title}
+			width={720}
+			open={open}
+			onClose={onClose}
+			styles={{
+				body: {
+					paddingBottom: 80,
+				},
+			}}
+		>
+			{title === "Edit Room" ? <Spin spinning={isLoading}>
+				<Form form={form} initialValues={dataEdit} layout="vertical" onFinish={handleUpdateRoom}>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="id"
+								label="Room No.: "
+								rules={[{ required: true, message: 'Please enter Room No.' }]}
+							>
+								<Input placeholder="Please enter Room No." />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="price"
+								label="Price/day ($): "
+								rules={[{ required: true, message: 'Please enter Price/day ($)' }]}
+							>
+								<Input type='number' placeholder="Please enter Price/day ($)" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="guest"
+								label="Guest: "
+								rules={[{ required: true, message: 'Please enter number of Guest' }]}
+							>
+								<Input type='number' placeholder="Please enter number of Guest" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="size"
+								label="Size: "
+								rules={[{ required: true, message: 'Please enter Size' }]}
+							>
+								<Input type='number' placeholder="Please enter Size" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Form.Item
+						name="image_url"
+						label="Image: "
+					>
+						<TextArea rows={2} />
+					</Form.Item>
+					<Form.Item
+						name="description"
+						label="Description: "
+					>
+						<TextArea rows={5} />
+					</Form.Item>
+					<Form.Item>
+						<Button htmlType='submit'>Submit</Button>
+					</Form.Item>
+				</Form>
+			</Spin> : <Spin spinning={isLoading}>
+				<Form form={form} layout="vertical" onFinish={handleAddRoom}>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="id"
+								label="Room No.: "
+								rules={[{ required: true, message: 'Please enter Room No.' }]}
+							>
+								<Input placeholder="Please enter Room No." />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="price"
+								label="Price/day ($): "
+								rules={[{ required: true, message: 'Please enter Price/day ($)' }]}
+							>
+								<Input type='number' placeholder="Please enter Price/day ($)" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="guest"
+								label="Guest: "
+								rules={[{ required: true, message: 'Please enter number of Guest' }]}
+							>
+								<Input type='number' placeholder="Please enter number of Guest" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="size"
+								label="Size: "
+								rules={[{ required: true, message: 'Please enter Size' }]}
+							>
+								<Input type='number' placeholder="Please enter Size" />
+							</Form.Item>
+						</Col>
+					</Row>
+					<Form.Item
+						name="image_url"
+						label="Image: "
+					>
+						<TextArea rows={2} />
+					</Form.Item>
+					<Form.Item
+						name="description"
+						label="Description: "
+					>
+						<TextArea rows={5} />
+					</Form.Item>
+					<Form.Item>
+						<Button htmlType='submit'>Submit</Button>
+					</Form.Item>
+				</Form>
+			</Spin>}
+		</Drawer>
+	)
+}
+
+export default DrawerRoom
