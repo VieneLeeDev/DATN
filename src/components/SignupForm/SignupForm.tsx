0ed86f4ac626e452d9bd05395836e414904cb314
VienLeeDev <levien26092k1@gmail.com>
2024-11-07 22:34:17 +0700

    update form sign up

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,75 +1,110 @@
 "use client";
-import { Button, Form, Input, notification, Space } from "antd";
+import { Button, Form, Input, notification, Space, Spin } from "antd";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import styles from "./SignupForm.module.css"
+import { useForm } from "antd/es/form/Form";
 export default function SignupForm() {
+	const [loading, setLoading] = useState<boolean>(false)
+	const [form] = useForm()
+	const handleSubmit = async () => {
+		const values = form.getFieldsValue();
+		const isValid = await form.validateFields()
+		if (isValid) {
+			try {
+				setLoading(true)
+				console.log(values)
+			} catch (error) {
+				console.log(error)
+			}
+			finally {
+				setTimeout(() => setLoading(false), 2000)
+			}
+		}
+	}
 	return (
-		<div className="flex justify-center items-center w-full h-full px-5">
-			<div className="w-full h-full max-w-[500px] max-h-[550px]">
-				<span className="block text-[45px] mb-[15px] font-cormorant font-[500]">Register</span>
-				<span className="block mb-[30px] text-[#777777] text-[18px]">Register for a hotel account to enjoy exclusive privileges</span>
-				<div className="w-full h-auto">
-					<Form layout="vertical" className={`${styles.Loginform} w-full h-full`}>
-					<Form.Item
-							name={"user_name"}
-							labelCol={{ span: 5 }}
-							rules={[
-								{
-									required: true,
-									message: "User name can not be empty!"
-								}
-							]}
-						>	<span className={styles["style-label"]}>User Name</span>
-							<Input className="h-[50px]"></Input>
-						</Form.Item>
-						<Form.Item
-							name={"email"}
-							labelCol={{ span: 5 }}
-							rules={[
-								{
-									required: true,
-									message: "Email can not be empty!"
-								}
-							]}
-						>	<span className={styles["style-label"]}>Email</span>
-							<Input className="h-[50px]" type="email"></Input>
-						</Form.Item>
-						<Form.Item
-							name={"password"}
-							labelCol={{ span: 5 }}
-							rules={[
-								{
-									required: true,
-									message: "Password can not be empty!"
-								}
-							]}
-						>
-							<span className={styles["style-label"]}>Password</span>
-							<Input className="h-[50px]" type="password"></Input>
-						</Form.Item>
-						<Form.Item
-							name={"repeat_password"}
-							labelCol={{ span: 5 }}
-							rules={[
-								{
-									required: true,
-									message: "Password can not be empty!"
-								}
-							]}
-						>
-							<span className={styles["style-label"]}>Repeat Password</span>
-							<Input className="h-[50px]" type="password"></Input>
-						</Form.Item>
-						<Button className={`${styles["submit-btn"]} w-full h-[50px] mt-[20px] text-[13px] font-serif uppercase bg-[#1C2C34] text-white font-[600]`}>Register Account</Button>
-					</Form> 
-					<Space className="flex mt-[50px]">
-						<span>Do you already have an account?</span>
-						<Link className="text-hoverbtn underline-offset-1" href={{pathname:"/login", query:{typeForm:"login"}}} >login</Link>
-						<Link className="text-hoverbtn underline-offset-1 ml-5" href={{pathname:'./',}} >Home page</Link>
-					</Space>
+		<Spin spinning={loading}>
+			<div className="flex justify-center items-center w-full h-full px-5">
+				<div className="w-full h-full max-w-[500px] max-h-[550px]">
+					<span className="block text-[45px] mb-[15px] font-cormorant font-[500]">Register</span>
+					<span className="block mb-[30px] text-[#777777] text-[18px]">Register for a hotel account to enjoy exclusive privileges</span>
+					<div className="w-full h-auto">
+						<Form form={form} onFinish={handleSubmit} layout="vertical" className={`${styles.Loginform} w-full h-full`}>
+							<Form.Item
+								labelCol={{ span: 5 }}
+							>	<span className={styles["style-label"]}>User Name</span>
+								<Form.Item name={"user_name"} rules={[
+									{
+										required: true,
+										message: "User name can not be empty!"
+									}
+								]}>
+									<Input className="h-[50px]" />
+								</Form.Item>
+							</Form.Item>
+							<Form.Item labelCol={{ span: 5 }}>
+								<span className={styles["style-label"]}>Email</span>
+								<Form.Item name={"email"} rules={[
+									{
+										required: true,
+										message: "Email can not be empty!"
+									}
+								]}>
+									<Input className="h-[50px]" type="email" />
+								</Form.Item>
+							</Form.Item>
+							<Form.Item labelCol={{ span: 5 }}>
+								<span className={styles["style-label"]}>Password</span>
+								<Form.Item name={"password"} rules={[
+									{
+										required: true,
+										message: "Password can not be empty!"
+									}
+								]}>
+									<Input.Password className="h-[50px]" />
+								</Form.Item>
+							</Form.Item>
+							<Form.Item
+
+								labelCol={{ span: 5 }}
+								rules={[
+									{
+										required: true,
+										message: "Password can not be empty!"
+									}
+								]}
+							>
+								<span className={styles["style-label"]}>Repeat Password</span>
+								<Form.Item
+									name={"repeat_password"}
+									rules={[
+										{
+											required: true,
+											message: 'Please confirm your password!',
+										},
+										({ getFieldValue }) => ({
+											validator(_, value) {
+												if (!value || getFieldValue('password') === value) {
+													return Promise.resolve();
+												}
+												return Promise.reject(new Error('The new password that you entered do not match!'));
+											},
+										}),
+									]}
+								>
+									<Input.Password className="h-[50px]" />
+								</Form.Item>
+							</Form.Item>
+							<Button onClick={form.submit} className={`${styles["submit-btn"]} w-full h-[50px] mt-[20px] text-[13px] font-serif uppercase bg-[#1C2C34] text-white font-[600]`}>Register Account</Button>
+						</Form>
+						<Space className="flex mt-[50px]">
+							<span>Do you already have an account?</span>
+							<Link className="text-hoverbtn underline-offset-1" href={{ pathname: "/login", query: { typeForm: "login" } }} >login</Link>
+							<Link className="text-hoverbtn underline-offset-1 ml-5" href={{ pathname: './', }} >Home page</Link>
+						</Space>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Spin >
 	);
 }
